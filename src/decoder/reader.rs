1ef7b424f8f3b5ed97029e74e010a1893aa7bdd7
Martin Larralde <martin.larralde@embl.de>
2023-10-07 00:30:48 +0200

    Implement sequence decoding for protein archives

--- src/decoder/reader.rs
+++ src/decoder/reader.rs
@@ -82,7 +82,7 @@ impl<R: BufRead> TextReader<R> {
 pub struct SequenceReader<R: BufRead> {
     reader: R,
     ty: SequenceType,
-    cache: Option<char>,
+    cache: Option<u8>,
 }
 
 impl<R: BufRead> SequenceReader<R> {
@@ -95,58 +95,73 @@ impl<R: BufRead> SequenceReader<R> {
     }
 
     pub fn next(&mut self, length: u64) -> Result<String, std::io::Error> {
-        if self.ty != SequenceType::Dna {
-            unimplemented!()
-        }
-
-        let mut sequence = String::with_capacity(length as usize);
-
+        let mut sequence = Vec::with_capacity(length as usize);
         if self.cache.is_some() && length > 0 {
             sequence.push(self.cache.take().unwrap());
         }
 
         while (sequence.len() as u64) < length {
-            let mut i = 0;
-            let buffer = self.reader.fill_buf()?;
-
-            while i < buffer.len() && (sequence.len() as u64) < length {
-                let c1 = Self::decode(buffer[i] & 0x0F);
-                sequence.push(c1);
-
-                let c2 = Self::decode(buffer[i] >> 4);
-                if sequence.len() as u64 == length {
-                    self.cache = Some(c2);
-                } else {
-                    sequence.push(c2);
-                }
-
-                i += 1;
-            }
-
-            self.reader.consume(i);
+            match self.ty {
+                SequenceType::Dna => self.read_nucleotide::<b'T'>(length, &mut sequence),
+                SequenceType::Rna => self.read_nucleotide::<b'U'>(length, &mut sequence),
+                SequenceType::Protein | SequenceType::Text => self.read_text(length, &mut sequence),
+            }?;
         }
 
-        Ok(sequence)
+        String::from_utf8(sequence)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))
     }
 
-    pub fn decode(c: u8) -> char {
+    fn read_text(&mut self, length: u64, sequence: &mut Vec<u8>) -> Result<(), std::io::Error> {
+        let buffer = self.reader.fill_buf()?;
+        
+        let n_to_copy = buffer.len().min(length as usize - sequence.len());
+        sequence.extend_from_slice(&buffer[..n_to_copy]);
+        self.reader.consume(n_to_copy);
+
+        Ok(())
+    }
+
+    fn read_nucleotide<const T: u8>(&mut self, length: u64, sequence: &mut Vec<u8>) -> Result<(), std::io::Error> {
+        let mut i = 0;
+        let buffer = self.reader.fill_buf()?;
+
+        while i < buffer.len() && (sequence.len() as u64) < length {
+            let c1 = Self::decode::<T>(buffer[i] & 0x0F);
+            sequence.push(c1);
+
+            let c2 = Self::decode::<T>(buffer[i] >> 4);
+            if sequence.len() as u64 == length {
+                self.cache = Some(c2);
+            } else {
+                sequence.push(c2);
+            }
+
+            i += 1;
+        }
+
+        self.reader.consume(i);
+        Ok(())
+    }
+
+    fn decode<const T: u8>(c: u8) -> u8 {
         match c {
-            0x00 => '-',
-            0x01 => 'T',
-            0x02 => 'G',
-            0x03 => 'K',
-            0x04 => 'C',
-            0x05 => 'Y',
-            0x06 => 'S',
-            0x07 => 'B',
-            0x08 => 'A',
-            0x09 => 'W',
-            0x0A => 'R',
-            0x0B => 'D',
-            0x0C => 'M',
-            0x0D => 'H',
-            0x0E => 'V',
-            0x0F => 'N',
+            0x00 => b'-',
+            0x01 => T,
+            0x02 => b'G',
+            0x03 => b'K',
+            0x04 => b'C',
+            0x05 => b'Y',
+            0x06 => b'S',
+            0x07 => b'B',
+            0x08 => b'A',
+            0x09 => b'W',
+            0x0A => b'R',
+            0x0B => b'D',
+            0x0C => b'M',
+            0x0D => b'H',
+            0x0E => b'V',
+            0x0F => b'N',
             _ => unreachable!(),
         }
     }
---
+++ tests/protein.rs
@@ -0,0 +1,26 @@
+extern crate naf;
+
+use naf::data::SequenceType;
+use naf::decoder::Decoder;
+
+#[test]
+fn decode() {
+
+    const ARCHIVE: &[u8] = include_bytes!("../data/LuxC.naf");
+
+    let c = std::io::Cursor::new(ARCHIVE);
+    let mut decoder = Decoder::new(c).unwrap();
+
+    assert_eq!(decoder.header.name_separator(), ' ');
+    assert_eq!(decoder.header.number_of_sequences(), 12);
+    assert_eq!(decoder.header.line_length(), 60);
+    assert_eq!(decoder.header.sequence_type(), SequenceType::Protein);    
+
+
+    let r1 = decoder.next().unwrap().unwrap();
+    assert!(r1.id.is_some());
+    assert!(r1.sequence.is_some());
+
+    let seq = r1.sequence.unwrap();
+    assert_eq!(seq.len(), 488);
+}
