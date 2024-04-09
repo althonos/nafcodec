bd6da76cbbba2364637252f44145f709f5298bde
Martin Larralde <martin.larralde@embl.de>
2024-04-09 15:54:55 +0200

    Add Python unit tests for the `Encoder` class

--- nafcodec-py/nafcodec/tests/__init__.py
+++ nafcodec-py/nafcodec/tests/__init__.py
@@ -1,6 +1,7 @@
-from . import test_decoder
+from . import test_decoder, test_encoder
 
 
 def load_tests(loader, suite, pattern):
     suite.addTests(loader.loadTestsFromModule(test_decoder))
+    suite.addTests(loader.loadTestsFromModule(test_encoder))
     return suite
---
+++ nafcodec-py/nafcodec/tests/test_encoder.py
@@ -0,0 +1,87 @@
+import gzip
+import io
+import os
+import tempfile
+import unittest
+
+import nafcodec
+from . import data
+
+try:
+    try:
+        from importlib.resources import files
+    except ImportError:
+        from importlib_resources import files  # type: ignore
+except ImportError:
+    files = None  # type: ignore
+
+
+class TestEncoder(unittest.TestCase):
+
+    def test_invalid_sequence(self):
+        buffer = io.BytesIO()
+        with self.assertRaises(ValueError):
+            encoder = nafcodec.Encoder(buffer, sequence_type="dna", sequence=True)
+            encoder.write(nafcodec.Record(sequence="hello world?!"))
+
+    def test_missing_field(self):
+        buffer = io.BytesIO()
+        with self.assertRaises(ValueError):
+            encoder = nafcodec.Encoder(buffer, sequence_type="dna", sequence=True)
+            encoder.write(nafcodec.Record())
+        with self.assertRaises(ValueError):
+            encoder = nafcodec.Encoder(buffer, sequence_type="dna", sequence=True)
+            encoder.write(nafcodec.Record(id="r1"))
+
+    def test_dna_records(self):
+        buffer = io.BytesIO()
+        
+        with nafcodec.Encoder(buffer, sequence_type="dna", id=True, sequence=True) as f:
+            f.write(nafcodec.Record(id="r1", sequence="ATTATTAGACAGAGC"))
+            f.write(nafcodec.Record(id="r2", sequence="CTATTG"))
+            f.write(nafcodec.Record(id="r3", sequence="TTAGTNNNNN"))
+        
+        buffer.seek(0)
+        decoder = nafcodec.Decoder(buffer)
+        records = list(decoder)
+        
+        self.assertEqual(len(records), 3)
+        self.assertEqual(records[0].id, "r1")
+        self.assertEqual(records[1].id, "r2")
+        self.assertEqual(records[2].id, "r3")
+        self.assertEqual(records[0].sequence, "ATTATTAGACAGAGC")
+        self.assertEqual(records[1].sequence, "CTATTG")
+        self.assertEqual(records[2].sequence, "TTAGTNNNNN")
+
+        for i in range(3):
+            self.assertIs(records[i].quality, None)
+            self.assertIs(records[i].comment, None)
+
+    def test_fastq_records(self):
+        buffer = io.BytesIO()
+        
+        with nafcodec.Encoder(buffer, sequence_type="rna", id=True, sequence=True, quality=True) as f:
+            f.write(nafcodec.Record(id="r1", sequence="AUUAU", quality="GGGGG"))
+            f.write(nafcodec.Record(id="r2", sequence="CUAUU", quality="#8A@C"))
+            f.write(nafcodec.Record(id="r3", sequence="UUAGU", quality="CCGGG"))
+        
+        buffer.seek(0)
+        decoder = nafcodec.Decoder(buffer)
+        records = list(decoder)
+        
+        self.assertEqual(len(records), 3)
+        self.assertEqual(records[0].id, "r1")
+        self.assertEqual(records[1].id, "r2")
+        self.assertEqual(records[2].id, "r3")
+        self.assertEqual(records[0].sequence, "AUUAU")
+        self.assertEqual(records[1].sequence, "CUAUU")
+        self.assertEqual(records[2].sequence, "UUAGU")
+        self.assertEqual(records[0].quality, "GGGGG")
+        self.assertEqual(records[1].quality, "#8A@C")
+        self.assertEqual(records[2].quality, "CCGGG")
+
+        for i in range(3):
+            self.assertIs(records[i].comment, None)
+
+
+
