1bd186240caba4279a0db6d1c1e1d699c7ac2080
Martin Larralde <martin.larralde@embl.de>
2025-01-31 15:16:06 +0100

    Fix setup of `built` and `pyo3-built` in `nafcodec-py`

--- nafcodec-py/nafcodec/build.rs
+++ nafcodec-py/nafcodec/build.rs
@@ -1,18 +1,5 @@
 extern crate built;
 
 fn main() {
-    let dst = std::env::var("OUT_DIR")
-        .map(std::path::PathBuf::from)
-        .expect("OUT_DIR not set")
-        .join("built.rs");
-    let project_dir = std::env::var("CARGO_MANIFEST_DIR")
-        .map(std::path::PathBuf::from)
-        .expect("CARGO_MANIFEST_DIR");
-
-    eprintln!(
-        "{:?}",
-        std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR")
-    );
-    built::write_built_file_with_opts(Some(&project_dir.parent().unwrap()), &dst)
-        .expect("Failed to acquire build-time information");
+    built::write_built_file().expect("Failed to acquire build-time information");
 }
